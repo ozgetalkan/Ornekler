c06671d79714cde55ab7616e6c689efa8ecfe841
özge alkan <ozgetanulkualkan@gmail.com>
2023-01-10 15:51:33 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary1
+{
+    public class Personel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        internal decimal Salary { get; set; } 
+    }
+}
---
+++ Ornek1.1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+enum GunEnum
+{
+    pazartesi,
+    sali,
+    carsamba,
+    persembe,
+    cuma,
+    cumartesi,
+    pazar
+}
+
+namespace Ornek1._1
+{
+    internal class Program
+    {
+        class Kisi
+        {
+            public Kisi(string ad, string soyad)
+            {
+                Ad = ad;
+                Soyad = soyad;
+            }
+            public String Ad { get; set; } 
+            public String Soyad { get; set; }   
+        }
+        static void Main(string[] args)
+        {
+            //Samp1(GunEnum.sali);
+        }
+        static void Samp1(GunEnum secilenGun)
+        {
+            switch (secilenGun)
+            {
+                case GunEnum.pazartesi:
+                    Console.WriteLine("Pazartesi");
+                    break;
+                case GunEnum.sali:
+                    Console.WriteLine("Salı");
+                    break;
+                case GunEnum.carsamba:
+                    Console.WriteLine("Çarşamba");
+                    break;
+                case GunEnum.persembe:
+                    Console.WriteLine("Perşembe");
+                    break;
+                case GunEnum.cuma:
+                    Console.WriteLine("Cuma");
+                    break;
+                default:
+                    Console.WriteLine("Haftasonu");
+                    break;
+            }
+        }
+
+        static void Samp2()
+        {
+            Kisi k1 = new Kisi("Veli", "Demir");
+            Kisi k2 = new Kisi("Ebru", "Zorba");
+
+            ArrayList list = new ArrayList();  
+            list.Add(k1);
+            list.Add(k2);
+
+            List<Kisi> kisiler = new List<Kisi>();
+            kisiler.Add(k1);
+            kisiler.Add(k2);
+
+            IEnumerator ie = kisiler.GetEnumerator();
+            while (ie.MoveNext())
+            {
+                Console.WriteLine(ie.Current.ToString());
+            }
+            do
+            {
+
+            }
+            while (true);
+        }
+    }
+}
